55a6e62f781357068f5bfb5191322c41edb4cdbb
Sreenithi <sreenithi.bala4@gmail.com>
2021-09-18 12:05:36 -0500

    Delete dataloader.py

--- dataloader.py
+++
@@ -1,6 +0,0 @@
-from keras.preprocessing.image import ImageDataGenerator
-datagen = ImageDataGenerator(width_shift_range=0.1,
-                             height_shift_range=0.1,
-                             zoom_range=0.2,
-                             shear_range=0.1,
-                             rotation_range=10)
